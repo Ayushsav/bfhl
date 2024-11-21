013c7467eaa424dbd280facf8b8de18f4ef38e6a
ayush <ayush@ayush.ayush>
2024-11-21 16:28:14 +0530

    21nov241

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,10 +6,12 @@ export default function Home() {
   const [response, setResponse] = useState(null);
 
   return (
+    <>
     <div>
       <h1>Your Roll Number: 0832CS211043</h1>
       <Form setResponse={setResponse} />
       {response && <Dropdown response={response} />}
     </div>
+    </>
   );
 }
